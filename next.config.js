eae3b150d929c456f7c8cc1d0d09dfdde2180bea
RaGnaRoK-thor <chowdhuryushnish2@gmail.com>
2025-10-14 13:01:04 +0000

    removed blog.html and links

--- next.config.js
+++ next.config.js
@@ -5,13 +5,11 @@ const nextConfig = {
     return [
       { source: '/', destination: '/index.html' },
       { source: '/about', destination: '/about.html' },
-      { source: '/blog', destination: '/blog.html' },
       { source: '/book-now', destination: '/book-now.html' },
       { source: '/contact', destination: '/contact.html' },
       { source: '/gallery', destination: '/gallery.html' },
       { source: '/our-policies', destination: '/our-policies.html' },
       { source: '/reviews', destination: '/reviews.html' },
-      { source: '/post/:slug', destination: '/post/:slug.html' },
       { source: '/resources/:file', destination: '/resources/:file.html' },
     ];
   },
